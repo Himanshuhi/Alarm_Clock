9514ea119f7bd130f82456c1a83daf74b0a7f5e2
Himanshu Yadav <97745121+Himanshuhi@users.noreply.github.com>
2023-03-14 20:44:19 +0530

    Update Alarm Clock.py

--- Alarm Clock.py
+++ Alarm Clock.py
@@ -1,6 +1,5 @@
 import time
 
-# Function to get user input for alarm time
 def get_alarm_time():
     while True:
         try:
@@ -13,20 +12,20 @@ def get_alarm_time():
         except ValueError:
             print("Invalid input. Please enter a valid integer hour and minute.")
 
-# Main program loop
+
 while True:
-    # Get current time
+    # himanshu
     current_time = time.localtime()
     current_hour, current_minute = current_time.tm_hour, current_time.tm_min
     
-    # Get user input for alarm time
+
     alarm_hour, alarm_minute = get_alarm_time()
     
-    # Wait until alarm time is reached
+
     while current_hour != alarm_hour or current_minute != alarm_minute:
         time.sleep(1)
         current_time = time.localtime()
         current_hour, current_minute = current_time.tm_hour, current_time.tm_min
         
-    # Play alarm sound (replace with your own code to play a sound)
+ 
     print("ALARM!")
